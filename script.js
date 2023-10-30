c14183be7493d452bf8de924805869891e698c45
M.C.Chaithanya <72519016+mc3007@users.noreply.github.com>
2023-10-30 14:00:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const billTotalInput = document.getElementById('billTotal');
+const totalBillInput = document.getElementById('totalBill');
 const tipPercentageInput = document.getElementById('tipPercentage');
 const tipAmountInput = document.getElementById('tipAmount');
 const totalWithTipInput = document.getElementById('totalWithTip');
@@ -7,12 +7,12 @@ const tipCalculatorForm = document.getElementById('tipCalculatorForm');
 tipCalculatorForm.addEventListener('input', calculateTip);
 
 function calculateTip() {
-    const billTotal = parseFloat(billTotalInput.value);
+    const totalBill = parseFloat(totalBillInput.value);
     const tipPercentage = tipPercentageInput.value;
-    const tipAmount = (billTotal * tipPercentage) / 100;
-    const totalWithTip = billTotal + tipAmount;
+    const tipAmount = (totalBill * tipPercentage) / 100;
+    const totalWithTip = totalBill + tipAmount;
 
-    if (!isNaN(billTotal)) {
+    if (!isNaN(totalBill)) {
         tipAmountInput.value = tipAmount.toFixed(2);
         totalWithTipInput.value = totalWithTip.toFixed(2);
     } else {
